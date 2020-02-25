2446e94767c77a0f0a5c1983c821475cf65b2c51
chepainais@gmail.com <chepa@chepa.lv>
2020-02-25 22:54:26 +0200

    Rebase uz origin

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include "EncoderCounter.h"
 
 const int green1Pin = 12;
 const int blue1Pin = 13;
@@ -7,9 +8,10 @@ const int blue2Pin = 13;
 
 unsigned MIN_POSITION = 0; // sould be set manually from controller (Hold programm button + btn1)
 unsigned MAX_POSITION = 2000; // sould be set manually from controller (Hold programm button + btn2)
+EncoderCounter encoder (green1Pin, blue1Pin);
 
-#include "encoder_counter.h"
-
+void ICACHE_RAM_ATTR l1gI();
+void ICACHE_RAM_ATTR l2bI();
 void setup()
 {
   // @TODO get last leg position from eeprom
@@ -21,16 +23,22 @@ void setup()
   pinMode(green2Pin, INPUT_PULLUP);
   pinMode(blue2Pin, INPUT_PULLUP);
   // ESP does not support multiple Interrupts to one pin (seccond one will overwrite the first one). So we should use only RISING interrupt
-  attachInterrupt(green1Pin, legg1greenRisingInterrupt, RISING);
-  attachInterrupt(blue1Pin, legg1blueRisingInterrupt, RISING);
+  attachInterrupt(green1Pin, l1gI, RISING);
+  attachInterrupt(blue1Pin, l2bI, RISING);
 }
-
+void ICACHE_RAM_ATTR   l1gI()
+{
+    encoder.greenRisingInterrupt();
+}
+void ICACHE_RAM_ATTR  l2bI()
+{
+    encoder.blueRisingInterrupt();
+}
 void loop()
 {
   Serial.write("Current position ");
-  Serial.print(CURRENT_LEFT_LEGG_POSITION);
+  Serial.print(encoder.CURRENT_POSITION);
   Serial.write("\n");
-  delay(200);
+  delay(1000);
   // put your main code here, to run repeatedly:
 }
-
